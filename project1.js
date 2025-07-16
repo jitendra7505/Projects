ab89d324925fdf7b8eb23044cbbf5bf119f5b0cf
jitendra <devlog172005@gmail.com>
2025-07-16 21:40:32 +0530

    Delete project1.js

--- project1.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('.toggle');
-const body = document.querySelector('body');
-
-toggle.addEventListener('click',() => toggle.classList.toggle("active"));
-
-toggle.addEventListener('click',()=> {
-    document.body.classList.toggle("darkMode");
-})
